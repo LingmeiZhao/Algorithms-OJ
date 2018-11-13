4595006a2f76fd8104ecf56a0188b383dbf51dcc
LingmeiZhao <ZhaoLingmei0@outlook.com>
2018-11-13 17:12:56 -0500

    add

---
+++ LeetCode/Easy/ReverseLinkedList.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+iteratively:
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+         ListNode* pre = NULL;
+        while (head) {
+            ListNode* next = head -> next;
+            head -> next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+};
+
+recursively:
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+          if (!head || !(head -> next)) {
+            return head;
+        }
+        ListNode* node = reverseList(head -> next);
+        head -> next -> next = head;
+        head -> next = NULL;
+        return node;
+    }
+};
